efe7c2831c640e7005c6f6d14b5b182c38b724dc
Florian Fuchs <flfuchs@redhat.com>
2016-04-08 13:59:45 +0200

    Adds a progress status for the current deployment
    Change-Id: I97827dd90a5c61c95a1334f58499e43d90b446a3

--- src/__tests__/components/deployment-plan/deployment-plan.tests.js
+++ src/__tests__/components/deployment-plan/deployment-plan.tests.js
@@ -1,17 +1,18 @@
-import { List, Map } from 'immutable';
+import { List } from 'immutable';
 
 import { mapStateToProps } from '../../../js/components/deployment-plan/DeploymentPlan.js';
+import { InitialPlanState } from '../../../js/immutableRecords/plans';
 
 describe('DeploymentPlan mapStateToProps', () => {
   describe('hasPlans flag', () => {
     it('returns ``hasPlans`` as `false`', () => {
-      let props = mapStateToProps({ plans: Map({
+      let props = mapStateToProps({ plans: InitialPlanState({
         all: List()
       })});
       expect(props.hasPlans).toBe(false);
     });
-    it('returns ``hasPlans`` as `false`', () => {
-      let props = mapStateToProps({ plans: Map({
+    it('returns ``hasPlans`` as `true`', () => {
+      let props = mapStateToProps({ plans: InitialPlanState({
         all: List(['foo', 'bar'])
       })});
       expect(props.hasPlans).toBe(true);
