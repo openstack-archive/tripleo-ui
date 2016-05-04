d725a18cb3e85f7f75082e2694a1bff1ed401f41
Jason E. Rist <jrist@redhat.com>
2016-05-04 08:55:47 -0600

    Refactor notification for better display
    - bring in CSS for patternfly
    - better style/alignment
    - notification self control
    - more than one notification at a time
    
    Change-Id: I0c173c80f5482f0813b673067b7bb297f2a610cc

--- src/js/components/notifications/NotificationsToaster.js
+++ src/js/components/notifications/NotificationsToaster.js
@@ -71,7 +71,7 @@ export default class NotificationsToaster extends React.Component {
   startTimer() {
     // Clear any previous timers
     this.clearTimer();
-    this.setState({timeout: setTimeout(this.toasterTimeout.bind(this), 8000)});
+    this.setState({timeout: setTimeout(this.toasterTimeout.bind(this), 89000)});
   }
 
   clearTimer() {
@@ -93,12 +93,11 @@ export default class NotificationsToaster extends React.Component {
 
   render() {
     return this.state.toasterNotification ? (
-      <div className="notification-toaster col-lg-5 col-md-6 col-sm-8 col-xs-12">
+      <div className="toast-pf-max-width toast-pf-top-right">
         <Notification
           title={this.state.toasterNotification.title}
           message={this.state.toasterNotification.message}
           type={this.state.toasterNotification.type}
-          dismissable
           removeNotification={this.closeNotification.bind(this)}
           onMouseEnter={this.notificationHover.bind(this, true)}
           onMouseLeave={this.notificationHover.bind(this, false)}/>
