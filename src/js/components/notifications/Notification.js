b997dfe54ba8ff01f6affa8e1f093f7ff6716a30
Jason E. Rist <jrist@redhat.com>
2016-05-04 08:55:47 -0600

    Refactor notification for better display
    - bring in CSS for patternfly
    - better style/alignment
    - notification self control
    - more than one notification at a time
    
    Change-Id: I0c173c80f5482f0813b673067b7bb297f2a610cc

--- src/js/components/notifications/Notification.js
+++ src/js/components/notifications/Notification.js
@@ -1,10 +1,56 @@
 import React from 'react';
 import ClassNames from 'classnames';
+import Timer from '../utils/Timer';
 
 export default class Notification extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      visible: true,
+      isMounted: true,
+      removed: false,
+      _notificationTimer: null
+    };
+  }
+
+  componentDidMount() {
+    console.log('timer start'); //eslint-disable-line no-console
+    this._notificationTimer = new Timer(() => {
+      this._hideNotification();
+    }, 8000);
+  }
+
+  _hideNotification() {
+    console.log('timer done'); //eslint-disable-line no-console
+    console.log('begin hide'); //eslint-disable-line no-console
+    if (this._notificationTimer) {
+      this._notificationTimer.clear();
+      this.props.removeNotification.bind(this)
+      console.log('clearNotificationTimer'); //eslint-disable-line no-console
+    }
+
+    if (this.isMounted) {
+      this.setState({
+        visible: false,
+        removed: true
+      });
+      console.log('unMounted'); //eslint-disable-line no-console
+    }
+  }
+
+  _handleMouseEnter() {
+    console.log('handle enter'); //eslint-disable-line no-console
+    this._notificationTimer.pause.bind(this);
+  }
+
+  _handleMouseLeave() {
+    console.log('handle leave'); //eslint-disable-line no-console
+    this._notificationTimer.resume.bind(this);
+  }
 
   render() {
     let classes = ClassNames({
+      'toast-pf': true,
       'alert': true,
       'alert-danger': this.props.type === 'error',
       'alert-warning': this.props.type === 'warning',
@@ -23,8 +69,8 @@ export default class Notification extends React.Component {
     return (
       <div className={classes}
            role="alert"
-           onMouseEnter={this.props.onMouseEnter}
-           onMouseLeave={this.props.onMouseLeave}>
+           onMouseEnter={this._handleMouseEnter}
+           onMouseLeave={this._handleMouseLeave}>
         <span className={iconClass} aria-hidden="true"></span>
         {this.props.dismissable ?
           <button type="button"
@@ -33,14 +79,19 @@ export default class Notification extends React.Component {
                   onClick={this.props.removeNotification.bind(this)}>
             <span className="pficon pficon-close" aria-hidden="true"></span>
           </button> : false}
-        <strong>{this.props.title}</strong> {this.props.message}
+        <strong>{this.props.title}</strong>
+        <p>{this.props.message}</p>
       </div>
     );
   }
 }
 Notification.propTypes = {
+  _handleMouseEnter: React.PropTypes.func,
+  _handleMouseLeave: React.PropTypes.func,
+  _hideNotification: React.PropTypes.func,
   dismissable: React.PropTypes.bool,
   message: React.PropTypes.string.isRequired,
+  notification: React.PropTypes.object,
   onMouseEnter: React.PropTypes.func,
   onMouseLeave: React.PropTypes.func,
   removeNotification: React.PropTypes.func,
