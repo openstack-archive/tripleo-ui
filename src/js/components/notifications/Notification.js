cd9cb44162460ba47c77e83e635fa884ee1a3766
Jason E. Rist <jrist@redhat.com>
2016-05-04 08:55:47 -0600

    Refactor notification for better display
    - bring in CSS for patternfly
    - better style/alignment
    - notification self control
    - more than one notification at a time
    
    Change-Id: I0c173c80f5482f0813b673067b7bb297f2a610cc

--- src/js/components/notifications/Notification.js
+++ src/js/components/notifications/Notification.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import ClassNames from 'classnames';
+import Timer from '../utils/Timer';
 
 export default class Notification extends React.Component {
+  constructor() {
+    super();
+    this._notificationTimer = null;
+  }
+
+  componentDidMount() {
+    this._notificationTimer = new Timer(() => {
+      this._hideNotification();
+    }, 8000);
+  }
+
+  _hideNotification() {
+    console.log('begin hide'); //eslint-disable-line no-console
+    if (this._notificationTimer) {
+      this._notificationTimer.clear();
+      this.props.removeNotification();
+    }
+  }
+
+  _handleMouseEnter() {
+    this._notificationTimer.pause();
+  }
+
+  _handleMouseLeave() {
+    this._notificationTimer.resume();
+  }
 
   render() {
     let classes = ClassNames({
+      'toast-pf': true,
       'alert': true,
+      'pull-right': true,
       'alert-danger': this.props.type === 'error',
       'alert-warning': this.props.type === 'warning',
       'alert-success': this.props.type === 'success',
@@ -23,23 +52,25 @@ export default class Notification extends React.Component {
     return (
       <div className={classes}
            role="alert"
-           onMouseEnter={this.props.onMouseEnter}
-           onMouseLeave={this.props.onMouseLeave}>
+           onMouseEnter={this._handleMouseEnter.bind(this)}
+           onMouseLeave={this._handleMouseLeave.bind(this)}>
         <span className={iconClass} aria-hidden="true"></span>
         {this.props.dismissable ?
           <button type="button"
                   className="close"
                   aria-label="Close"
-                  onClick={this.props.removeNotification.bind(this)}>
+                  onClick={this._hideNotification.bind(this)}>
             <span className="pficon pficon-close" aria-hidden="true"></span>
           </button> : false}
-        <strong>{this.props.title}</strong> {this.props.message}
+        <strong>{this.props.title}</strong>
+        <p>{this.props.message}</p>
       </div>
     );
   }
 }
+
 Notification.propTypes = {
-  dismissable: React.PropTypes.bool,
+  dismissable: React.PropTypes.bool.isRequired,
   message: React.PropTypes.string.isRequired,
   onMouseEnter: React.PropTypes.func,
   onMouseLeave: React.PropTypes.func,
@@ -47,8 +78,8 @@ Notification.propTypes = {
   title: React.PropTypes.string,
   type: React.PropTypes.string
 };
+
 Notification.defaultProps = {
-  dismissable: false,
   title: '',
   type: 'error'
 };
