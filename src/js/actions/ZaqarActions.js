6e44c233ad113f94aed62c6542c69fc8a5236694
Jiri Tomasek <jtomasek@redhat.com>
2016-05-16 11:28:06 +0200

    Initial zaqar websockets
    Adds ZaqarWebSocketService that handles the connection, any incomming
    message triggers messageReceived action which takes care of dispatching
    the messages and triggering the actions dependent on the message
    
    The WebSocket connection is established on successful login
    
    Change-Id: I3f1e9c5acf0a58edc63ff21c222b2730e123b988

---
+++ src/js/actions/ZaqarActions.js
@@ -0,0 +1,33 @@
+import * as _ from 'lodash';
+import MistralApiService from '../services/MistralApiService';
+import NotificationActions from './NotificationActions';
+
+export default {
+  sendZaqarMessage() {
+    return (dispatch, getState) => {
+      MistralApiService.runAction('zaqar.queue_post',
+                                  {queue_name: 'tripleo',
+                                   messages: [{body:'I am a message a new one'}]});
+    };
+  },
+
+  messageReceived(message) {
+    return (dispatch, getState) => {
+      switch (true) {
+
+      case (_.includes(['authenticate',
+                       'queue_create',
+                       'subscription_create'], message.request.action)):
+        dispatch(NotificationActions.notify({ type: 'success',
+                                              title: 'Zaqar',
+                                              message: message.body.message}));
+        break;
+
+      default:
+        dispatch(NotificationActions.notify({ type: 'success',
+                                              title: 'Zaqar',
+                                              message: message.body.message}));
+      }
+    };
+  }
+};
