df0c01ea3524f6e8174118338d9cf5a08cdafb2a
Jiri Tomasek <jtomasek@redhat.com>
2016-05-31 15:15:33 +0200

    Register nodes new workflow
    Change-Id: Ia711d2693ff559f3a16752403657853a2d3c83c4

--- src/js/actions/ZaqarActions.js
+++ src/js/actions/ZaqarActions.js
@@ -1,10 +1,33 @@
+import * as _ from 'lodash';
+import NotificationActions from './NotificationActions';
+import RegisterNodesActions from './RegisterNodesActions';
+
 export default {
   messageReceived(message) {
     return (dispatch, getState) => {
       switch (true) {
 
-      default:
+      case (message.request &&
+            _.includes(['authenticate', 'subscription_create'], message.request.action)):
+        dispatch(NotificationActions.notify({ type: 'success',
+                                              title: 'Zaqar',
+                                              message: message.body.message }));
         break;
+
+      case (message.request && message.request.action === 'queue_create'):
+        dispatch(NotificationActions.notify({ type: 'success',
+                                              title: 'Zaqar',
+                                              message: message.body }));
+        break;
+
+      case (message.body.type === 'tripleo.baremetal.v1.register'):
+        dispatch(RegisterNodesActions.nodesRegistrationFinished(message.body.payload));
+        break;
+
+      default:
+        dispatch(NotificationActions.notify({ type: 'success',
+                                              title: 'Zaqar',
+                                              message: JSON.stringify(message.body) }));
       }
     };
   }
