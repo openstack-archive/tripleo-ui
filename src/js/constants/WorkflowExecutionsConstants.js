36627639115c168349fb87d1f5badfcfc2edbf0f
Jiri Tomasek <jtomasek@redhat.com>
2016-06-22 18:20:35 +0200

    List Validations and Validation results
    This change implements new Validations and Results listing based on Mistral
    driven backend. Validations are listed using ListValidations Mistral action.
    Results of the Validation are workflow executions of run_validation
    workflows where input is Validation's name. Those 2 sources are combined
    using selectors.
    
    Implements: blueprint validations-mistral-integration
    Change-Id: Icac754491a4198f529c599eff413bb85d8bb5014

---
+++ src/js/constants/WorkflowExecutionsConstants.js
@@ -0,0 +1,7 @@
+import keyMirror from 'keymirror';
+
+export default keyMirror({
+  FETCH_WORKFLOW_EXECUTIONS_PENDING: null,
+  FETCH_WORKFLOW_EXECUTIONS_SUCCESS: null,
+  FETCH_WORKFLOW_EXECUTIONS_FAILED: null
+});
--- src/js/reducers/validationsReducer.js
+++ src/js/reducers/validationsReducer.js
@@ -1,70 +1,32 @@
-import { fromJS, Map, List } from 'immutable';
+import { fromJS, Map } from 'immutable';
 
 import ValidationsConstants from '../constants/ValidationsConstants';
-import { Validation, ValidationResult, ValidationStage } from '../immutableRecords/validations';
+import { Validation } from '../immutableRecords/validations';
 
 const initialState = Map({
-  loaded: false,
+  validationsLoaded: false,
   isFetching: false,
-  validationStages: Map(),
-  validations: Map(),
-  validationResults: Map()
+  validations: Map()
 });
 
 export default function validationsReducer(state = initialState, action) {
   switch(action.type) {
 
-  case ValidationsConstants.FETCH_VALIDATION_STAGES_PENDING: {
+  case ValidationsConstants.FETCH_VALIDATIONS_PENDING: {
     return state.set('isFetching', true);
   }
 
-  case ValidationsConstants.FETCH_VALIDATION_STAGES_SUCCESS: {
-    const validations = fromJS(action.payload.entities.validations) || Map();
-    const validationStages = fromJS(action.payload.entities.validationStages) || Map();
-    const validationResults = fromJS(action.payload.entities.validationResults) || Map();
+  case ValidationsConstants.FETCH_VALIDATIONS_SUCCESS: {
+    const validations = fromJS(action.payload);
 
-    return state.set('validationStages', validationStages
-                  .map(stage => new ValidationStage(stage.set('visible',
-                    state.getIn(['validationStages', stage.uuid, 'visible'], false)))))
-                .set('validations', validations
-                  .map(validation => new Validation(validation)))
-                .set('validationResults', validationResults
-                  .map(result => new ValidationResult(result)))
+    return state.set('validations', validations.map( validation => new Validation(validation)))
                 .set('isFetching', false)
-                .set('loaded', true);
+                .set('validationsLoaded', true);
   }
 
-  case ValidationsConstants.FETCH_VALIDATION_STAGES_FAILED:
+  case ValidationsConstants.FETCH_VALIDATIONS_FAILED:
     return state.set('isFetching', false)
-                .set('loaded', true);
-
-  case ValidationsConstants.UPDATE_STAGE_STATUS: {
-    let validationIds = state.getIn(
-      ['validationStages', action.payload.uuid, 'validations'],
-      List([]));
-
-    let validations = state.get('validations');
-
-    let updatedValidations = validationIds.reduce((r, validationId) => {
-      return r.setIn([validationId, 'status'], action.payload.status);
-    }, validations);
-
-    return state.setIn(['validationStages', action.payload.uuid, 'status'], action.payload.status)
-                .set('validations', updatedValidations);
-  }
-
-  case ValidationsConstants.UPDATE_VALIDATION_STATUS: {
-    return state.setIn(['validations', action.payload.uuid, 'status'], action.payload.status);
-  }
-
-  case ValidationsConstants.TOGGLE_VALIDATION_STAGE_VISIBILITY: {
-    return state.updateIn(['validationStages', action.payload.uuid, 'visible'],
-                          (oldValue) => !oldValue);
-  }
-
-  case ValidationsConstants.SHOW_VALIDATION_STAGE: {
-    return state.setIn(['validationStages', action.payload.uuid, 'visible'], true);
-  }
+                .set('validationsLoaded', true);
 
   default:
     return state;
