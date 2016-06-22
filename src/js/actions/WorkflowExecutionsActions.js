c547e1707e0fa035dfafbba783aceaabe39992b5
Jiri Tomasek <jtomasek@redhat.com>
2016-06-22 18:20:35 +0200

    List Validations and Validation results
    This change implements new Validations and Results listing based on Mistral
    driven backend. Validations are listed using ListValidations Mistral action.
    Results of the Validation are workflow executions of run_validation
    workflows where input is Validation's name. Those 2 sources are combined
    using selectors.
    
    Implements: blueprint validations-mistral-integration
    Change-Id: Icac754491a4198f529c599eff413bb85d8bb5014

---
+++ src/js/actions/WorkflowExecutionsActions.js
@@ -0,0 +1,50 @@
+import { normalize, arrayOf } from 'normalizr';
+// import { fromJS, List, Map } from 'immutable';
+// import when from 'when';
+
+// import IronicApiErrorHandler from '../services/IronicApiErrorHandler';
+// import IronicApiService from '../services/IronicApiService';
+import MistralApiService from '../services/MistralApiService';
+import MistralApiErrorHandler from '../services/MistralApiErrorHandler';
+import WorkflowExecutionsConstants from '../constants/WorkflowExecutionsConstants';
+import NotificationActions from './NotificationActions';
+import { workflowExecutionSchema } from '../normalizrSchemas/workflowExecutions';
+
+export default {
+  fetchWorkflowExecutions() {
+    return (dispatch, getState) => {
+      dispatch(this.fetchWorkflowExecutionsPending());
+      MistralApiService.getWorkflowExecutions()
+      .then((response) => {
+        const executions = normalize(response.executions,
+                                     arrayOf(workflowExecutionSchema)).entities.executions || {};
+        dispatch(this.fetchWorkflowExecutionsSuccess(executions));
+      }).catch((error) => {
+        let errorHandler = new MistralApiErrorHandler(error);
+        errorHandler.errors.forEach((error) => {
+          dispatch(NotificationActions.notify(error));
+        });
+        dispatch(this.fetchWorkflowExecutionsFailed());
+      });
+    };
+  },
+
+  fetchWorkflowExecutionsPending() {
+    return {
+      type: WorkflowExecutionsConstants.FETCH_WORKFLOW_EXECUTIONS_PENDING
+    };
+  },
+
+  fetchWorkflowExecutionsSuccess(executions) {
+    return {
+      type: WorkflowExecutionsConstants.FETCH_WORKFLOW_EXECUTIONS_SUCCESS,
+      payload: executions
+    };
+  },
+
+  fetchWorkflowExecutionsFailed() {
+    return {
+      type: WorkflowExecutionsConstants.FETCH_WORKFLOW_EXECUTIONS_FAILED
+    };
+  }
+};
--- src/js/normalizrSchemas/validations.js
+++ src/js/normalizrSchemas/validations.js
@@ -1,13 +1,15 @@
-import { Schema, arrayOf } from 'normalizr';
+import { Schema } from 'normalizr';
 
-export const validationStageSchema = new Schema('validationStages', { idAttribute: 'uuid' });
-export const validationSchema = new Schema('validations', { idAttribute: 'uuid' });
-export const validationResultSchema = new Schema('validationResults', { idAttribute: 'uuid' });
+// export const validationStageSchema = new Schema('validationStages', { idAttribute: 'uuid' });
+// export const validationSchema = new Schema('validations', { idAttribute: 'uuid' });
+// export const validationResultSchema = new Schema('validationResults', { idAttribute: 'uuid' });
+//
+// validationStageSchema.define({
+//   validations: arrayOf(validationSchema)
+// });
+//
+// validationSchema.define({
+//   latest_result: validationResultSchema
+// });
 
-validationStageSchema.define({
-  validations: arrayOf(validationSchema)
-});
-
-validationSchema.define({
-  latest_result: validationResultSchema
-});
+export const validationSchema = new Schema('validations', { idAttribute: 'id' });
