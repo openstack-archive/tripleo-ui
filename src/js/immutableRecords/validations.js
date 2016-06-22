b4302a9696973e4a7edf1cc35278c14f068c94c1
Jiri Tomasek <jtomasek@redhat.com>
2016-06-22 18:20:35 +0200

    List Validations and Validation results
    This change implements new Validations and Results listing based on Mistral
    driven backend. Validations are listed using ListValidations Mistral action.
    Results of the Validation are workflow executions of run_validation
    workflows where input is Validation's name. Those 2 sources are combined
    using selectors.
    
    Implements: blueprint validations-mistral-integration
    Change-Id: Icac754491a4198f529c599eff413bb85d8bb5014

--- src/js/immutableRecords/validations.js
+++ src/js/immutableRecords/validations.js
@@ -1,35 +1,11 @@
 import { List, Map, Record } from 'immutable';
 
-export const ValidationStage = Record({
-  description: '',
-  name: '',
-  visible: false,
-  stage: '',
-  status: '',
-  uuid: '',
-  validations: List()
-});
-
 export const Validation = Record({
   description: '',
-  latest_result: undefined,
-  name: '',
-  status: '',
-  results: List(),
-  uuid: ''
-});
-
-export const ValidationResult = Record({
-  date: undefined,
-  detailed_description: Map(),
-  status: undefined,
-  uuid: ''
-});
-
-export const ValidationsStatusCounts = Record({
-  new: 0,
-  running: 0,
-  success: 0,
-  error: 0,
-  failed: 0
+  groups: List(),
+  id: undefined,
+  metadata: undefined,
+  name: undefined,
+  results: Map(),
+  status: undefined
 });
