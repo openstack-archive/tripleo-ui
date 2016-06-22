3a75ef439994ac263e2b1565ae1bfa2bf8bffb5c
Jiri Tomasek <jtomasek@redhat.com>
2016-06-22 18:20:35 +0200

    List Validations and Validation results
    This change implements new Validations and Results listing based on Mistral
    driven backend. Validations are listed using ListValidations Mistral action.
    Results of the Validation are workflow executions of run_validation
    workflows where input is Validation's name. Those 2 sources are combined
    using selectors.
    
    Implements: blueprint validations-mistral-integration
    Change-Id: Icac754491a4198f529c599eff413bb85d8bb5014

--- src/js/actions/ValidationsActions.js
+++ src/js/actions/ValidationsActions.js
@@ -1,22 +1,27 @@
 import { normalize, arrayOf } from 'normalizr';
 
+import MistralApiService from '../services/MistralApiService';
 import NotificationActions from './NotificationActions';
+import MistralApiErrorHandler from '../services/MistralApiErrorHandler';
 import ValidationsApiService from '../services/ValidationsApiService';
 import ValidationsApiErrorHandler from '../services/ValidationsApiErrorHandler';
 import ValidationsConstants from '../constants/ValidationsConstants';
-import { validationStageSchema } from '../normalizrSchemas/validations';
+import { validationSchema } from '../normalizrSchemas/validations';
 
 export default {
-  fetchValidationStages() {
+  fetchValidations() {
     return (dispatch, getState) => {
-      dispatch(this.fetchValidationStagesPending());
-      ValidationsApiService.getStages().then((response) => {
-        response = normalize(response, arrayOf(validationStageSchema));
-        dispatch(this.fetchValidationStagesSuccess(response));
+      dispatch(this.fetchValidationsPending());
+      MistralApiService.runAction('tripleo.list_validations').then((response) => {
+
+        const actionResult = JSON.parse(response.output).result;
+        const validations = normalize(actionResult,
+                                      arrayOf(validationSchema)).entities.validations || {};
+        dispatch(this.fetchValidationsSuccess(validations));
       }).catch((error) => {
-        console.error('Error in ValidationActions.fetchValidationStages', error.stack || error); //eslint-disable-line no-console
-        dispatch(this.fetchValidationStagesFailed());
-        let errorHandler = new ValidationsApiErrorHandler(error);
+        console.error('Error in ValidationActions.fetchValidations', error.stack || error); //eslint-disable-line no-console
+        dispatch(this.fetchValidationsFailed());
+        let errorHandler = new MistralApiErrorHandler(error);
         errorHandler.errors.forEach((error) => {
           dispatch(NotificationActions.notify(error));
         });
@@ -24,66 +29,22 @@ export default {
     };
   },
 
-  fetchValidationStagesPending() {
+  fetchValidationsPending() {
     return {
-      type: ValidationsConstants.FETCH_VALIDATION_STAGES_PENDING
+      type: ValidationsConstants.FETCH_VALIDATIONS_PENDING
     };
   },
 
-  fetchValidationStagesSuccess(stages) {
+  fetchValidationsSuccess(validations) {
     return {
-      type: ValidationsConstants.FETCH_VALIDATION_STAGES_SUCCESS,
-      payload: stages
+      type: ValidationsConstants.FETCH_VALIDATIONS_SUCCESS,
+      payload: validations
     };
   },
 
-  fetchValidationStagesFailed() {
+  fetchValidationsFailed() {
     return {
-      type: ValidationsConstants.FETCH_VALIDATION_STAGES_FAILED
-    };
-  },
-
-  runValidationStage(uuid) {
-    return dispatch => {
-      dispatch(this.updateValidationStageStatus(uuid, 'running'));
-      dispatch(this.showValidationStage(uuid));
-      ValidationsApiService.runStage(uuid).then((response) => {
-      }).catch((error) => {
-        console.error('Error in ValidationStage.runStage', error.stack); //eslint-disable-line no-console
-        let errorHandler = new ValidationsApiErrorHandler(error);
-        errorHandler.errors.forEach((error) => {
-          dispatch(NotificationActions.notify(error));
-        });
-        dispatch(this.updateValidationStageStatus(uuid, 'error'));
-      });
-    };
-  },
-
-  toggleValidationStageVisibility(uuid) {
-    return {
-      type: ValidationsConstants.TOGGLE_VALIDATION_STAGE_VISIBILITY,
-      payload: {
-        uuid
-      }
-    };
-  },
-
-  showValidationStage(uuid) {
-    return {
-      type: ValidationsConstants.SHOW_VALIDATION_STAGE,
-      payload: {
-        uuid
-      }
-    };
-  },
-
-  updateValidationStageStatus(uuid, status) {
-    return {
-      type: ValidationsConstants.UPDATE_STAGE_STATUS,
-      payload: {
-        uuid,
-        status
-      }
+      type: ValidationsConstants.FETCH_VALIDATIONS_FAILED
     };
   },
 
