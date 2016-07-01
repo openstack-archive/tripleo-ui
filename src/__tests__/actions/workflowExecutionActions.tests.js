4d227f7f1a55c7c2618aa39b1e1c412859bd1346
Jiri Tomasek <jtomasek@redhat.com>
2016-07-01 15:12:05 +0200

    Stop Validation
    When Validation is running, user can stop it at any time by clicking
    Validation status icon. Doing so updates the workflow execution to
    'PAUSED' state. The status of the validation is determined as
    new, so user is still obliged to run it later
    
    Validations and Workflow Executions tests added
    
    Implements: blueprint validations-mistral-integration
    Change-Id: Ied030d410615e02dbf21777d4be7f2c1124f0db9

---
+++ src/__tests__/actions/workflowExecutionActions.tests.js
@@ -0,0 +1,63 @@
+import when from 'when';
+
+import MistralApiService from '../../js/services/MistralApiService';
+import WorkflowExecutionsActions from '../../js/actions/WorkflowExecutionsActions';
+
+let createResolvingPromise = (data) => {
+  return () => {
+    return when.resolve(data);
+  };
+};
+
+describe('fetchWorkflowExecutions action', () => {
+  beforeEach(done => {
+    spyOn(WorkflowExecutionsActions, 'fetchWorkflowExecutionsPending');
+    spyOn(WorkflowExecutionsActions, 'fetchWorkflowExecutionsSuccess');
+
+    const response = {
+      executions: [{
+        state: 'SUCCESS',
+        params: '{}',
+        output: '{\'status\': \'FAILED\',}',
+        input: '{\'validation_name\': \'check-network-gateway\'}',
+        id: '1a'
+      }]
+    };
+
+    spyOn(MistralApiService, 'getWorkflowExecutions').and.callFake(
+      createResolvingPromise(response)
+    );
+
+    WorkflowExecutionsActions.fetchWorkflowExecutions()(() => {}, () => {});
+    setTimeout(() => { done(); }, 1);
+  });
+
+  it('dispatches appropriate actions and normalizes the response', () => {
+    expect(WorkflowExecutionsActions.fetchWorkflowExecutionsPending).toHaveBeenCalled();
+    expect(MistralApiService.getWorkflowExecutions).toHaveBeenCalled();
+    expect(WorkflowExecutionsActions.fetchWorkflowExecutionsSuccess).toHaveBeenCalled();
+  });
+});
+
+describe('updateWorkflowExecution action', () => {
+  beforeEach(done => {
+    spyOn(WorkflowExecutionsActions, 'updateWorkflowExecutionPending');
+    spyOn(WorkflowExecutionsActions, 'addWorkflowExecution');
+
+    spyOn(MistralApiService, 'updateWorkflowExecution').and.callFake(
+      createResolvingPromise()
+    );
+
+    WorkflowExecutionsActions.updateWorkflowExecution('512e',
+                                                      { state: 'PAUSED' })(() => {}, () => {});
+    setTimeout(() => { done(); }, 1);
+  });
+
+  it('dispatches appropriate actions', () => {
+    expect(WorkflowExecutionsActions.updateWorkflowExecutionPending)
+      .toHaveBeenCalledWith('512e', { state: 'PAUSED' });
+    expect(MistralApiService.updateWorkflowExecution)
+      .toHaveBeenCalledWith('512e', { state: 'PAUSED' });
+    expect(WorkflowExecutionsActions.addWorkflowExecution).toHaveBeenCalled();
+  });
+});
