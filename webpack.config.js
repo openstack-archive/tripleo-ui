c0806635037cf4c132087d3051d51fe69fd754d7
Jiri Tomasek <jtomasek@redhat.com>
2016-07-12 10:08:24 +0200

    Webpack
    Change-Id: I00142c8c33b5b7e043024c51d68321908c9e9cb8

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  devtool: 'source-map',
+  entry: __dirname + '/src/js/index.js',
   output: {
+    path: __dirname + '/dist',
     filename: 'tripleo_ui.js',
-    sourceMapFilename: '../js/tripleo_ui.js.map'
+    sourceMapFilename: 'tripleo_ui.js.map'
   },
   module: {
     loaders: [
@@ -10,12 +11,38 @@ module.exports = {
         test: /\.js$/,
         include: /src/,
         exclude: /src\/js\/workers/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'stage-0', 'react']
-        }
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.js$/,
+        include: /src\/js\/workers/,
+        loader: 'shared-worker!babel-loader'
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader?limit=8192' // inline base64 URLs for <=8k images, direct URLs for the rest
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-url-loader?noquotes'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.less$/,
+        loader: 'style-loader!css-loader!less-loader'
       }
     ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist',
+    colors: true,
+    historyApiFallback: true,
+    inline: true
+
   },
   node: {
     fs: 'empty',
