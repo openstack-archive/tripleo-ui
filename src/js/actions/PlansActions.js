b1dc18115b75211aac80e7fe7a9b032bbeef7d83
Jiri Tomasek <jtomasek@redhat.com>
2016-08-18 13:11:33 +0200

    Migrate Deploy action to Mistral
    Change-Id: Ic79826684023bfb89cd32f98e162f5b8877cc0c2

--- src/js/actions/PlansActions.js
+++ src/js/actions/PlansActions.js
@@ -291,7 +291,8 @@ export default {
   deployPlan(planName) {
     return dispatch => {
       dispatch(this.deployPlanPending(planName));
-      TripleOApiService.deployPlan(planName).then((response) => {
+      MistralApiService.runAction('tripleo.deploy', { container: planName, timeout: 240 })
+      .then((response) => {
         dispatch(this.deployPlanSuccess(planName, response));
         dispatch(StackActions.fetchStacks());
         dispatch(NotificationActions.notify({
@@ -301,7 +302,7 @@ export default {
         }));
       }).catch(error => {
         dispatch(this.deployPlanFailed(planName, error));
-        let errorHandler = new TripleOApiErrorHandler(error);
+        let errorHandler = new MistralApiErrorHandler(error);
         errorHandler.errors.forEach((error) => {
           dispatch(NotificationActions.notify(error));
         });
