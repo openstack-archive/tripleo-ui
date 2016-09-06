1ee30f2aa782731a0b58dc2669a0a637dd021526
Jiri Tomasek <jtomasek@redhat.com>
2016-09-06 09:51:38 +0200

    Implement Validation Detail modal
    Implements Validation Detail modal, so it is possible to view
    Validation status, description and output. Enables user to debug
    failing Validation.
    
    Implements: blueprint validation-detail
    Change-Id: Ia936be7b7669c699b1886076ff99485d62b8664b

---
+++ src/js/components/validations/ValidationDetail.js
@@ -0,0 +1,99 @@
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import React from 'react';
+
+import Modal from '../ui/Modal';
+import { ValidationStatusIcon } from './ValidationStatusIcon';
+
+export default class ValidationDetail extends React.Component {
+  constructor() {
+    super();
+    this.state = { isPending: false };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ isPending: false });
+  }
+
+  /**
+   * Decide which validation action to run, if validation is pending (waits for execution to be
+   * started) no action can be run
+   */
+  triggerValidationAction() {
+    this.setState({ isPending: true });
+
+    switch (true) {
+    case (this.state.isPending):
+      break;
+    case (this.props.status === 'running'):
+      this.props.stopValidation(this.props.results.last().id);
+      break;
+    default:
+      this.props.runValidation();
+      break;
+    }
+  }
+
+  renderValidationGroups() {
+    return this.props.groups.map(group => {
+      return (
+        <small key={group}>
+          <span className="label label-default">{group}</span>
+          &nbsp;
+        </small>
+      );
+    });
+  }
+
+  renderValidationOutput() {
+    const lastResult = this.props.results.last();
+    if (lastResult && this.props.status !== 'running') {
+      return (
+        <div>
+          <p><strong>Output:</strong></p>
+          <pre>{lastResult.output.get('stdout', lastResult.output.get('result'))}</pre>
+        </div>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <Modal dialogClasses="modal-lg">
+        <div className="modal-header">
+          <button type="button"
+                  className="close"
+                  aria-label="Close"
+                  onClick={this.props.hideValidationDetail}>
+            <span aria-hidden="true" className="pficon pficon-close"/>
+          </button>
+          <h4 className="modal-title">Validation Detail</h4>
+        </div>
+        <div className="modal-body">
+          <div className="validation-detail-title">
+            <div className="list-view-pf-left">
+              <ValidationStatusIcon
+                status={this.state.isPending ? 'running' : this.props.status}
+                triggerValidationAction={this.triggerValidationAction.bind(this)}/>
+            </div>
+            <h3>{this.props.name}</h3>
+          </div>
+          <p><strong>Description:</strong> <br/>{this.props.description}</p>
+          <p><strong>Groups:</strong> {this.renderValidationGroups()}</p>
+          <p><strong>Status:</strong> {this.props.status}</p>
+          {this.renderValidationOutput()}
+        </div>
+      </Modal>
+    );
+  }
+}
+
+ValidationDetail.propTypes = {
+  description: React.PropTypes.string,
+  groups: ImmutablePropTypes.list.isRequired,
+  hideValidationDetail: React.PropTypes.func,
+  name: React.PropTypes.string.isRequired,
+  results: ImmutablePropTypes.map.isRequired,
+  runValidation: React.PropTypes.func.isRequired,
+  status: React.PropTypes.string.isRequired,
+  stopValidation: React.PropTypes.func.isRequired
+};
---
+++ src/js/components/validations/ValidationStatusIcon.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { includes } from 'lodash';
+import ClassNames from 'classnames';
+
+export const ValidationStatusIcon = ({ status, triggerValidationAction }) => {
+  const statusIconClass = ClassNames({
+    'list-view-pf-icon-md':               true,
+    'running fa fa-stop-circle':          status === 'running',
+    'pficon pficon-error-circle-o front': status === 'failed',
+    'pficon pficon-ok front':             status === 'success',
+    'fa fa-play-circle':                  includes(['new', 'paused'], status),
+    'pficon pficon-help':                 status === 'error'
+  });
+
+  const runValidationIconClass = 'list-view-pf-icon-md fa fa-play-circle back';
+
+  switch (true) {
+  case (includes(['new', 'running', 'paused'], status)):
+    return (
+      <a className="link"
+         onClick={triggerValidationAction}>
+        <span className={statusIconClass}/>
+      </a>
+    );
+  case (includes(['success', 'failed'], status)):
+    return (
+      <a className="link flip-container"
+         onClick={triggerValidationAction}>
+        <div className="flipper">
+          <span className={statusIconClass}/>
+          <span className={runValidationIconClass}/>
+        </div>
+      </a>
+    );
+  default:
+    return (
+      <a className="link flip-container"
+         onClick={triggerValidationAction}>
+        <div className="flipper">
+          <span className={statusIconClass}/>
+          <span className={runValidationIconClass}/>
+        </div>
+      </a>
+    );
+  }
+};
+ValidationStatusIcon.propTypes = {
+  status: React.PropTypes.string,
+  triggerValidationAction: React.PropTypes.func.isRequired
+};
