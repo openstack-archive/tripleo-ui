c7a70e63268724a974c09a058eb4c99a37e0494d
Jiri Tomasek <jtomasek@redhat.com>
2016-10-20 17:09:31 +0200

    Fix overflow scrolling
    Rendering modal component caused hiding scrolbars. This change
    makes sure the Modal is mounted only when we want the Modal to
    be displayed
    
    Change-Id: I9e7b4e6dae47af5c928f2b945c5d7c5d3c80cd8b

--- src/js/components/ui/Modal.js
+++ src/js/components/ui/Modal.js
@@ -2,6 +2,25 @@ import React from 'react';
 import Portal from 'react-portal';
 
 export default class Modal extends React.Component {
+  render() {
+    return (
+      <Portal isOpened={this.props.show}>
+        <PortedModal {...this.props} />
+      </Portal>
+    );
+  }
+}
+Modal.propTypes = {
+  show: React.PropTypes.bool.isRequired
+};
+
+Modal.defaultProps = {
+  show: true
+};
+
+// This component is intended for internal use of Modal Component
+// It is used to mount Modal only when it should be shown
+class PortedModal extends React.Component {
   // add the modal-open class to the body of the page so scrollbars render properly.
   componentWillMount() {
     document.body.classList.add('modal-open');
@@ -14,29 +33,24 @@ export default class Modal extends React.Component {
 
   render() {
     return (
-      <Portal isOpened>
-        <div style={{display: this.props.show ? 'block' : 'none'}}>
-          <div className="modal modal-visible" role="dialog">
-            <div className={`modal-dialog ${this.props.dialogClasses}`}>
-              <div className="modal-content">
-                {this.props.children}
-              </div>
+      <div style={{display: 'block'}}>
+        <div className="modal modal-visible" role="dialog">
+          <div className={`modal-dialog ${this.props.dialogClasses}`}>
+            <div className="modal-content">
+              {this.props.children}
             </div>
           </div>
-          <div className="modal-backdrop in"></div>
         </div>
-      </Portal>
+        <div className="modal-backdrop in"></div>
+      </div>
     );
   }
 }
-
-Modal.propTypes = {
+PortedModal.propTypes = {
   children: React.PropTypes.node,
-  dialogClasses: React.PropTypes.string.isRequired,
-  show: React.PropTypes.bool.isRequired
+  dialogClasses: React.PropTypes.string.isRequired
 };
 
-Modal.defaultProps = {
-  dialogClasses: '',
-  show: true
+PortedModal.defaultProps = {
+  dialogClasses: ''
 };
