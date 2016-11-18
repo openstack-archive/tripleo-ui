c0fff5e9f289e9eded9e8811341880428da708ba
Florian Fuchs <flfuchs@redhat.com>
2016-11-18 15:44:26 +0100

    Adds basic internationalization support
    This patch adds an I18NProvider component to implement react-intl's
    translation mechanisms and replaces text strings in the deployment_plan
    components with formatted messages.
    
    The patch also contains scripts to convert extracted messages to `.pot`
    files (which can be used with zanata.org) and back. See more details in
    the README changes.
    
    More components need to be updated once this patch has landed.
    
    Change-Id: Ida9fc65c65bedf377341220be1d7225d1ae58b2c
    Implements: blueprint tripleo-ui-i18n-support-for-js

--- src/js/components/deployment_plan/NoPlans.js
+++ src/js/components/deployment_plan/NoPlans.js
@@ -1,5 +1,21 @@
+import { defineMessages, FormattedMessage } from 'react-intl';
 import { Link } from 'react-router';
 import React from 'react';
+
+const messages = defineMessages({
+  noPlansAvailable: {
+    id: 'NoPlans.noPlansAvailable',
+    defaultMessage: 'No Deployment Plans Available'
+  },
+  noPlansAvailableMessage: {
+    id: 'NoPlans.noPlansAvailableMessage',
+    defaultMessage: 'There are no Deployment Plans available. Please create one first.'
+  },
+  createNewPlan: {
+    id: 'NoPlans.createNewPlan',
+    defaultMessage: 'Create New Plan'
+  }
+});
 
 export default class NoPlans extends React.Component {
   render() {
@@ -8,13 +24,13 @@ export default class NoPlans extends React.Component {
         <div className="blank-slate-pf-icon">
           <span className="fa fa-ban"></span>
         </div>
-        <h1>No Deployment Plans Available</h1>
-        <p>There are no Deployment Plans available. Please create one first.</p>
+        <h1><FormattedMessage {...messages.noPlansAvailable}/></h1>
+        <p><FormattedMessage {...messages.noPlansAvailableMessage}/></p>
         <div className="blank-slate-pf-main-action">
           <Link to="/plans/new"
                 query={{tab: 'newPlan'}}
                 className="btn btn-lg btn-primary">
-            <span className="fa fa-plus"/> Create New Plan
+            <span className="fa fa-plus"/> <FormattedMessage {...messages.createNewPlan}/>
           </Link>
         </div>
       </div>
--- src/js/components/deployment_plan/PlansDropdown.js
+++ src/js/components/deployment_plan/PlansDropdown.js
@@ -1,10 +1,22 @@
+import { defineMessages, FormattedMessage } from 'react-intl';
 import ImmutablePropTypes from 'react-immutable-proptypes';
-import React from 'react';
 import { Link } from 'react-router';
+import React from 'react';
 
 import DropdownItem from '../ui/dropdown/DropdownItem';
 import DropdownButton from '../ui/dropdown/DropdownButton';
 import Dropdown from '../ui/dropdown/Dropdown';
+
+const messages = defineMessages({
+  manageDeployments: {
+    id: 'PlansDropdown.manageDeployments',
+    defaultMessage: 'Manage Deployments'
+  },
+  selectDeployment: {
+    id: 'PlansDropdown.selectDeployment',
+    defaultMessage: 'Select Deployment'
+  }
+});
 
 export default class PlansDropdown extends React.Component {
   renderRecentPlans() {
@@ -21,15 +33,21 @@ export default class PlansDropdown extends React.Component {
   render() {
     if(this.props.plans.isEmpty()) {
       return (
-        <Link className="btn btn-link" to="/plans/list">Manage Deployments</Link>
+        <Link className="btn btn-link" to="/plans/list">
+          <FormattedMessage {...messages.manageDeployments}/>
+        </Link>
       );
     } else {
       return (
         <Dropdown>
-          <DropdownButton className="btn-link">Select Deployment</DropdownButton>
+          <DropdownButton className="btn-link">
+            <FormattedMessage {...messages.selectDeployment}/>
+          </DropdownButton>
           {this.renderRecentPlans()}
           <DropdownItem key="divider" divider/>
-          <DropdownItem key="plansLink" to="/plans/list">Manage Deployments</DropdownItem>
+          <DropdownItem key="plansLink" to="/plans/list">
+            <FormattedMessage {...messages.manageDeployments}/>
+          </DropdownItem>
         </Dropdown>
       );
     }
