565ac45d4bf9740986cc5483760fac123be7c940
Florian Fuchs <flfuchs@redhat.com>
2016-12-16 15:17:47 +0100

    Adds language selector to NavBar
    This patch adds:
    
    1. Action, reducer and component changes to switch the language.
    
    2. A DropdownToggle component that is generic than the existing Dropdown
       component, so it can be used inside the NavBar component.
    
    3. CSS changes for the NavBar dropdown.
    
    Implements: blueprint tripleo-ui-i18n-support-for-js
    
    Change-Id: I83947ef8716c0be522b4eae763ef12b1343dc0a3

--- src/js/components/i18n/I18nProvider.js
+++ src/js/components/i18n/I18nProvider.js
@@ -1,8 +1,11 @@
 import { addLocaleData, IntlProvider } from 'react-intl';
+import { connect } from 'react-redux';
 import React from 'react';
 import ja from 'react-intl/locale-data/ja';
 
+import I18nActions from '../../actions/I18nActions';
 import jaMessages from '../../../../i18n/locales/ja.json';
+import { getLanguage } from '../../selectors/i18n';
 
 
 const MESSAGES = {
@@ -14,25 +17,15 @@ class I18nProvider extends React.Component {
   constructor() {
     super();
     addLocaleData([...ja]);
-    this.state = {
-      locale: 'en'
-    };
   }
 
-  componentWillMount() {
-    const locale = localStorage.getItem('language') ||
-                   (navigator.languages && navigator.languages[0]) ||
-                   navigator.language || navigator.userLanguage;
-    // We only use the country part of the locale:
-    const language = locale.substr(0, 2);
-    if(MESSAGES[language]) {
-      this.setState({ locale: language });
-    }
+  componentDidMount() {
+    this.props.detectLanguage();
   }
 
   render() {
     return (
-      <IntlProvider locale={this.state.locale} messages={MESSAGES[this.state.locale]}>
+      <IntlProvider locale={this.props.language} messages={MESSAGES[this.props.language]}>
         {this.props.children}
       </IntlProvider>
     );
@@ -40,7 +33,21 @@ class I18nProvider extends React.Component {
 }
 
 I18nProvider.propTypes = {
-  children: React.PropTypes.node
+  children: React.PropTypes.node,
+  detectLanguage: React.PropTypes.func.isRequired,
+  language: React.PropTypes.string
 };
 
-export default I18nProvider;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    detectLanguage: () => dispatch(I18nActions.detectLanguage())
+  };
+};
+
+const mapStateToProps = (state) => {
+  return {
+    language: getLanguage(state)
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(I18nProvider);
