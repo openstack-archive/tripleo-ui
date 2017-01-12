e92cc305b410f278c9d034874e31f76a8ec46c64
Florian Fuchs <flfuchs@redhat.com>
2017-01-12 14:40:54 +0100

    [WIP] Add i18n to Notifications
    Adding i18n to the Notifications is a little different to other
    components because the title and message are defined outside the
    component, often coming from external web service calls.
    
    This patch adds an extra thunk parameter which can be used inside
    actions to translate strings.
    
    Change-Id: I6335c6ccb92e3077ba6400fa6364446a757863eb

--- src/js/selectors/i18n.js
+++ src/js/selectors/i18n.js
@@ -1,4 +1,5 @@
 import { createSelector } from 'reselect';
+import { IntlProvider } from 'react-intl';
 
 const languageSelector = state => state.i18n.get('language');
 
@@ -11,3 +12,11 @@ export const getLanguage = createSelector(
 export const getMessages = createSelector(
   [languageSelector, messagesSelector], (language, messages) => messages[language]
 );
+
+export const getIntl = createSelector(
+  [languageSelector, messagesSelector], (language, messages) => {
+    const intlProvider = new IntlProvider({ locale: language, messages: messages[language] }, {});
+    const { intl } = intlProvider.getChildContext();
+    return intl;
+  }
+)
