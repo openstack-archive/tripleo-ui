6f2ccc32c6b63988b3aeb14f6c72ed2804810988
Honza Pokorny <honza@redhat.com>
2017-02-02 10:12:11 -0400

    Remove dist/ directory
    * Move dist/index.html to src/
    * Move dist/tripleo_ui_config.js.samle to config/
    * Use html webpack plugin to generate dist/index.html on build
    * Update documentation
    
    Reasons:
    
    * The dist/ dir conflicts with Python builds in CI
    * Semantically, having the two files in that directory doesn't make sense
    
    Closes-bug: 1661261
    Change-Id: I0904e9e5c2ced12865611fab21ee1f66d33d52bb

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 require('es6-promise').polyfill(); // https://github.com/webpack/css-loader/issues/144
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devtool: 'inline-source-map',
@@ -9,6 +10,11 @@ module.exports = {
     filename: 'tripleo_ui.js',
     sourceMapFilename: 'tripleo_ui.js.map'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
   module: {
     loaders: [
       // Javascript
