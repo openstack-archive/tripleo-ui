d3e7b1b0edc98ece0e7369621551de6a7b835853
Jiri Tomasek <jtomasek@redhat.com>
2017-02-15 14:51:24 +0100

    Add HorizontalNumberInput
    HorizontalNumberInput converts value string to number
    
    Closes-Bug: #1664975
    Change-Id: I29a401739b3a45c7ba4fb808c46d8909ac58ef4e
    (cherry picked from commit 28db59e8b60aa0bad81265e7bd0c51f430922334)

---
+++ src/js/components/ui/forms/HorizontalNumberInput.js
@@ -0,0 +1,76 @@
+import ClassNames from 'classnames';
+import Formsy from 'formsy-react';
+import React from 'react';
+
+import InputDescription from './InputDescription';
+import InputErrorMessage from './InputErrorMessage';
+
+class HorizontalNumberInput extends React.Component {
+  changeValue(event) {
+    event.stopPropagation(); // https://github.com/christianalfoni/formsy-react/issues/203
+    this.props.setValue(isNaN(parseInt(event.target.value)) ?
+                          undefined : parseInt(event.target.value));
+  }
+
+  // https://github.com/christianalfoni/formsy-react/issues/332
+  getValue() {
+    if(this.props.getValue()) {
+      return this.props.getValue();
+    }
+    return 0;
+  }
+
+  render() {
+    let divClasses = ClassNames({
+      'form-group': true,
+      'has-error': this.props.showError(),
+      'required': this.props.isRequired()
+    });
+
+    return (
+      <div className={divClasses}>
+        <label htmlFor={this.props.name}
+               className={`${this.props.labelColumnClasses} control-label`}>
+          {this.props.title}
+        </label>
+        <div className={this.props.inputColumnClasses}>
+          <input type="number"
+                 name={this.props.name}
+                 ref={this.props.name}
+                 id={this.props.name}
+                 className="form-control"
+                 onChange={this.changeValue.bind(this)}
+                 value={this.getValue()}
+                 placeholder={this.props.placeholder}
+                 min={this.props.min}
+                 max={this.props.max}
+                 disabled={this.props.disabled} />
+          <InputErrorMessage getErrorMessage={this.props.getErrorMessage} />
+          <InputDescription description={this.props.description} />
+        </div>
+      </div>
+    );
+  }
+}
+HorizontalNumberInput.propTypes = {
+  description: React.PropTypes.string,
+  disabled: React.PropTypes.bool,
+  getErrorMessage: React.PropTypes.func,
+  getValue: React.PropTypes.func,
+  inputColumnClasses: React.PropTypes.string.isRequired,
+  isRequired: React.PropTypes.func,
+  isValid: React.PropTypes.func,
+  labelColumnClasses: React.PropTypes.string.isRequired,
+  max: React.PropTypes.number,
+  min: React.PropTypes.number,
+  name: React.PropTypes.string.isRequired,
+  placeholder: React.PropTypes.string,
+  setValue: React.PropTypes.func,
+  showError: React.PropTypes.func,
+  title: React.PropTypes.string.isRequired
+};
+HorizontalNumberInput.defaultProps = {
+  inputColumnClasses: 'col-sm-10',
+  labelColumnClasses: 'col-sm-2'
+};
+export default Formsy.HOC(HorizontalNumberInput);
