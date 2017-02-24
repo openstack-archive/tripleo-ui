a6a49a5146dcffc6b9249acc72a6d978716e1be3
Jason E. Rist <jrist@redhat.com>
2017-02-23 17:07:44 -0700

    Adds tooltips to the deployment plan page.
    Change-Id: I029275bb5ccd7d7491a7a251081f70498624f211
    Closes-bug: #1648888

--- src/js/components/deployment_plan/DeploymentPlanStep.js
+++ src/js/components/deployment_plan/DeploymentPlanStep.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-export const DeploymentPlanStep = ({ children, disabled, title }) => {
+export const DeploymentPlanStep = ({ children, disabled, title, tooltip }) => {
   return (
     <li className={disabled ? 'disabled' : null}>
       <h3>
         <span>{title}</span>
+        { tooltip ?
+          <span is data-tooltip={tooltip} class="tooltip-right">
+            <span className="pficon pficon-info"/>
+          </span>
+          : null
+        }
       </h3>
       {children}
     </li>
@@ -14,9 +20,11 @@ export const DeploymentPlanStep = ({ children, disabled, title }) => {
 DeploymentPlanStep.propTypes = {
   children: React.PropTypes.node,
   disabled: React.PropTypes.bool,
-  title: React.PropTypes.string.isRequired
+  title: React.PropTypes.string.isRequired,
+  tooltip: React.PropTypes.string
 };
 
 DeploymentPlanStep.defaultProps = {
-  disabled: false
+  disabled: false,
+  tooltip: ''
 };
