b6b1059b2c3e2addc325d08445a2b45bcfe6acfc
Honza Pokorny <honza@redhat.com>
2017-03-03 14:58:20 -0400

    Add language switcher to login page
    Closes-Bug: #1667064
    Change-Id: Ifa5d8f3a19b77ddfe0a8e3b0d1dba61582065719

---
+++ src/js/components/ui/forms/LanguageInput.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Formsy from 'formsy-react';
+import { injectIntl } from 'react-intl';
+import I18nActions from '../../../actions/I18nActions';
+import { getAppConfig } from '../../../services/utils';
+
+class LanguageInput extends React.Component {
+
+  _renderOptions() {
+    const configLanguages = getAppConfig().languages || {};
+    const langList = Object.keys(configLanguages).sort((a , b) =>
+      configLanguages[a] > configLanguages[b]);
+
+    return langList.map((lang) => {
+      return (
+      <option key={`lang-${lang}`} value={lang}>
+        {configLanguages[lang]}
+      </option>
+      );
+    });
+  }
+
+  _onChange(event) {
+    this.props.chooseLanguage(event.currentTarget.value);
+  }
+
+  render() {
+    return (
+      <div className="form-group">
+        <label className="col-sm-2 col-md-2 control-label" htmlFor={this.props.name}>
+        </label>
+        <div className="col-sm-4 col-sm-offset-6 col-md-4 col-md-offset-6">
+          <select className="combobox form-control" name={this.props.name}
+            value={this.props.language}
+            autoFocus={this.props.autoFocus}
+            onChange={this._onChange.bind(this)}>
+            {this._renderOptions()}
+          </select>
+        </div>
+      </div>
+    );
+  }
+}
+
+LanguageInput.propTypes = {
+  autoFocus: React.PropTypes.bool,
+  chooseLanguage: React.PropTypes.func.isRequired,
+  getErrorMessage: React.PropTypes.func,
+  getValue: React.PropTypes.func,
+  language: React.PropTypes.string,
+  name: React.PropTypes.string.isRequired,
+  setValue: React.PropTypes.func
+};
+
+LanguageInput.defaultProps = {
+  autoFocus: false
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    chooseLanguage: (language) => dispatch(I18nActions.chooseLanguage(language))
+  };
+};
+
+export default injectIntl(connect(null, mapDispatchToProps)(LanguageInput));
