a2225d5141e681690cd285417a59611511ecc183
Florian Fuchs <flfuchs@redhat.com>
2017-03-08 15:03:38 +0100

    Fix nodes actions toolbar
    An additional span inside the nodes toolbar, added by the i18n
    FormattedMessage component, breaks the toolbar by changing the click
    event target to the span element; because the span element doesn't
    contain the "name" attribute needed to determine the appropriate action.
    
    This patch adds a small method to lookup the closest name attribute
    (either on the element or the parent elements), so both buttons with and
    without a FormattedMessage of them will work.
    
    Change-Id: I7c565da8bf2f5040ff99b13f02cb0a7f4c07637f
    Closes-Bug: #1671100
    (cherry picked from commit 8d40ed773b08575a6d8345e116049203e204fa43)

---
+++ src/js/components/utils/Dom.js
@@ -0,0 +1,7 @@
+export function findClosestWithAttribute(element, attrName) {
+  // If the element has a name attr, return it.
+  // Otherwise try the parent element.
+  if(element) {
+    return element[attrName] || findClosestWithAttribute(element.parentElement, attrName);
+  }
+}
