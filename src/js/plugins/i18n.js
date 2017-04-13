aaab08b0f1d5683c76fa8758df6524efdee33a44
Honza Pokorny <honza@redhat.com>
2017-04-13 11:28:16 -0300

    Automatically enable all available languages
    Right now, we have to create at least two patches for each new language that
    becomes available: one to update the tripleo-ui itself, and one to update the
    defaults in the puppet code.  This is painful and takes a long time.
    
    This patch turns things around.  Instead of specifying which languages should be
    available, we automatically load all of them, and provide users with a way of
    disabling some of them should they need to.  When a new language is imported
    from the translation team into our code, there is no work necessary.
    
    This is accomplished via Webpack plugin which reads the i18n/locales directory,
    parses the data, and generates a Javascript file with the necessary imports and
    other useful objects.  The "languages" setting in the config file is changed to
    "excludedLanguages", and it's a simple list of language abbreviations.
    
    Closes-Bug: #1682452
    Change-Id: Idf5a3314c19be18ca6cabbae1e94bc7cb1d1fe94

---
+++ src/js/plugins/i18n.js
@@ -0,0 +1,77 @@
+/* I18nPlugin for Webpack
+ * ======================
+ *
+ * This plugin is used to automatically enable all available languages.  It
+ * looks in the "localePath" configuration variable (which in our case is
+ * "i18n/locales"), and reads the files in that directory.  It then generates a
+ * Javascript file with all the necessary imports and other useful objects.
+ * This file is placed in "src/js/components/i18n/messages.js".  You can see a
+ * sample of this file below.
+ *
+ * import deMessages from '../../../../i18n/locales/de.json';
+ * import esMessages from '../../../../i18n/locales/es.json';
+ *
+ * import de from 'react-intl/locale-data/de';
+ * import es from 'react-intl/locale-data/es';
+ *
+ * export const MESSAGES = {
+ *   'de': deMessages['de'],
+ *   'es': esMessages['es']
+ * };
+ *
+ *
+ * export const LOCALE_DATA = [...de, ...es];
+ */
+
+var fs = require('fs');
+
+var I18nPlugin = function(options) {
+  this.options = options;
+};
+
+I18nPlugin.prototype.apply = function(compiler) {
+
+  var files = fs.readdirSync(this.options.localePath);
+  var locales = files.map(function(file) { return file.replace('.json', '');});
+
+  var localesLower = locales.map(function(locale) {
+    return locale.toLowerCase().replace('-', '');
+  });
+
+  var localesSimple = locales.map(function(locale) {
+    return locale.split('-')[0];
+  });
+
+  var messagesImport = localesSimple.map(function(locale, index) {
+    var lower = localesLower[index];
+    var full = locales[index];
+    return 'import ' + lower + 'Messages from \'../../../../i18n/locales/' +
+      full + '.json\';';
+  }).join('\n');
+
+  var reactIntlImport = localesSimple.map(function(locale, index) {
+    return 'import ' + locale + ' from \'react-intl/locale-data/' + locale + '\';';
+
+  }).join('\n');
+
+  var messages = locales.map(function(locale, index) {
+    var lower = localesLower[index];
+    return '\'' + locale + '\': ' + lower + 'Messages[\'' + locale + '\']';
+  }).join(',\n  ');
+
+  var messagesObj = 'export const MESSAGES = {\n  ' + messages + '\n}';
+
+  var localeData = localesSimple.map(function(locale) {
+    return '...' + locale;
+  }).join(', ');
+
+  var localeDataObj = 'export const LOCALE_DATA = [' + localeData + '];';
+
+  var file = messagesImport + '\n\n' + reactIntlImport + '\n\n' + messagesObj +
+      '\n\n'+ localeDataObj;
+
+  fs.writeFileSync('src/js/components/i18n/messages.js', file);
+
+};
+
+module.exports = I18nPlugin;
