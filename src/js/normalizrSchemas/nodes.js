ae6b355a20cc9498d6aaabcbeffcca134d9c976f
Jiri Tomasek <jtomasek@redhat.com>
2017-04-25 12:42:40 +0200

    Simplify Node Ports fetching
    This change introduces using Ironic API to fetch all ports
    and combine them with Nodes. This greatly improves performance
    of fetching Nodes, as it now requires 2 API calls rather than
    N+1.
    
    Change-Id: I2e38b8b8b948c20e05da6f846bf236b7633c7b85

--- src/js/normalizrSchemas/nodes.js
+++ src/js/normalizrSchemas/nodes.js
@@ -1,8 +1,4 @@
-import { arrayOf, Schema } from 'normalizr';
+import { Schema } from 'normalizr';
 
 export const nodeSchema = new Schema('nodes', { idAttribute: 'uuid' });
 export const portSchema = new Schema('ports', { idAttribute: 'uuid' });
-
-nodeSchema.define({
-  portsDetail: arrayOf(portSchema)
-});
