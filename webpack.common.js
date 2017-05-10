8d47410fe55f8021080823f05a972ef431f836e2
Honza Pokorny <honza@redhat.com>
2017-05-10 13:41:16 -0300

    Run minification and tree shaking on build
    "npm run build" now builds an artifact optimized for production use.  The
    removes dead code, and minizes the result.  The size of the bundle shrinks from
    about 14MB to 2MB.
    
    The previous build command is moved to "npm run build:dev".
    
    Change-Id: I21430f06049c4eeeb4a66c7143da8c2919fb5a24
    Closes-Bug: 1688610

---
+++ webpack.common.js
@@ -0,0 +1,97 @@
+require('es6-promise').polyfill(); // https://github.com/webpack/css-loader/issues/144
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: __dirname + '/src/js/index.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'tripleo_ui.js',
+    sourceMapFilename: 'tripleo_ui.js.map'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      // Javascript
+      {
+        test: /\.js$/,
+        include: /src/,
+        exclude: /src\/js\/workers/,
+        loader: 'babel-loader'
+      },
+
+      // Images
+      {
+        test: /\.(png|jpg|gif)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: 8192, // inline base64 URLs for <=8k images, direct URLs for the rest
+          name: '[name].[ext]'
+        }
+      },
+
+      // Fonts and svg images
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: 8192,
+          mimetype: 'application/font-woff',
+          name: '[name].[ext]'
+        }
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: 8192,
+          mimetype: 'application/octet-stream',
+          name: '[name].[ext]'
+        }
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+        query: { name: '[name].[ext]' }
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: 8192,
+          mimetype: 'image/svg+xml',
+          name: '[name].[ext]'
+        }
+      },
+
+      // Plain CSS files
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+
+      // Less
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader?sourceMap'
+        ]
+      },
+
+      {
+        loader: __dirname + '/src/js/loaders/version.js',
+        test: /src\/js\/index.js$/
+      }
+
+    ]
+  }
+};
