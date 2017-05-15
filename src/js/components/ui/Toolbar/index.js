8ad62c8ade4d37890aff3653c7d152897e6d14e6
Jiri Tomasek <jtomasek@redhat.com>
2017-05-15 15:29:04 +0200

    TableView toolbar styling
    * Add missing patternfly-additions styles to base.less
    * introduce tableView Toolbar prop to apply toolbar + table styling
    * use tableView Toolbar in NodesToolbar and ValidationsToolbar
    
    Change-Id: Ieb1e05ef5284e7bb72746a79bb4aec98156955fb

--- src/js/components/ui/Toolbar/index.js
+++ src/js/components/ui/Toolbar/index.js
@@ -1,19 +1,33 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-export const Toolbar = ({ children }) => {
-  return (
-    <div className="container-fluid">
-      <div className="toolbar-pf row">
+export const Toolbar = ({ children, tableView }) => {
+  if (tableView) {
+    return (
+      <div className="toolbar-pf row table-view-pf-toolbar">
         <div className="col-sm-12">
           {children}
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="container-fluid">
+        <div className="toolbar-pf row">
+          <div className="col-sm-12">
+            {children}
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 Toolbar.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
+  tableView: PropTypes.bool.isRequired
+};
+Toolbar.defaultProps = {
+  tableView: false
 };
 
 export const ToolbarActions = ({ children }) => (
