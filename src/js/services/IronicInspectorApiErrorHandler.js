aa84f54f21b21b23fb38a4872ec71a4dbad83cf6
Jiri Tomasek <jtomasek@redhat.com>
2017-05-31 14:52:13 +0200

    Show Node Introspection Data
    * Fetches introspection data on expanding node
    * If introspection data are available, display them in NodeExtendedInfo
    
    Change-Id: I5feac2b64803e1cf2c31f0bbc259940a80ddae3c

---
+++ src/js/services/IronicInspectorApiErrorHandler.js
@@ -0,0 +1,74 @@
+/**
+ * Copyright 2017 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+import BaseHttpRequestErrorHandler
+  from '../components/utils/BaseHttpRequestErrorHandler';
+import LoginActions from '../actions/LoginActions';
+import store from '../store';
+
+export default class IronicInspectorApiErrorHandler
+  extends BaseHttpRequestErrorHandler {
+  _generateErrors(errorObj) {
+    let errors = [];
+    let error;
+    // A weak check to find out if it's not an xhr object.
+    if (!errorObj.status && errorObj.message) {
+      errors.push({
+        title: 'Error',
+        message: errorObj.message
+      });
+      return errors;
+    }
+    switch (errorObj.status) {
+      case 0:
+        errors.push({
+          title: 'Connection Error',
+          message: 'Connection to Ironic Inspector is not available'
+        });
+        break;
+      case 404:
+        error = JSON.parse(errorObj.responseText).error.message;
+        errors.push({
+          title: 'Introspection data not found',
+          message: error
+        });
+        break;
+      case 401:
+        error = JSON.parse(errorObj.responseText).error.message;
+        errors.push({
+          title: 'Unauthorized',
+          message: error
+        });
+        store.dispatch(LoginActions.logoutUser());
+        break;
+      default:
+        error = JSON.parse(errorObj.responseText).error.message;
+        status = errorObj.status;
+        errors.push({
+          title: `Error ${status}`,
+          message: error
+        });
+        break;
+    }
+    return errors;
+  }
+
+  // TODO(jtomasek): remove this, I am leaving this here just for example reasons
+  // this function should be implemented by form related subclass.
+  _generateFormFieldErrors() {
+    return {};
+  }
+}
