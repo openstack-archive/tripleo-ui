d24207eed2562627adfbb9d0baa7b3e274f726cc
Jiri Tomasek <jtomasek@redhat.com>
2017-05-31 14:52:13 +0200

    Show Node Introspection Data
    * Fetches introspection data on expanding node
    * If introspection data are available, display them in NodeExtendedInfo
    
    Change-Id: I5feac2b64803e1cf2c31f0bbc259940a80ddae3c

--- src/js/components/nodes/NodesListView/NodesListView.js
+++ src/js/components/nodes/NodesListView/NodesListView.js
@@ -1,4 +1,21 @@
+/**
+ * Copyright 2017 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 import ImmutablePropTypes from 'react-immutable-proptypes';
+import PropTypes from 'prop-types';
 import React from 'react';
 
 import { ListView } from '../../ui/ListView';
@@ -10,12 +27,12 @@ export default class NodesListView extends React.Component {
       <ListView>
         {this.props.nodes
           .toList()
-          .toJS()
           .map(node => (
             <NodeListItem
+              fetchNodeIntrospectionData={this.props.fetchNodeIntrospectionData}
               node={node}
-              key={node.uuid}
-              inProgress={this.props.nodesInProgress.includes(node.uuid)}
+              key={node.get('uuid')}
+              inProgress={this.props.nodesInProgress.includes(node.get('uuid'))}
             />
           ))}
       </ListView>
@@ -23,6 +40,7 @@ export default class NodesListView extends React.Component {
   }
 }
 NodesListView.propTypes = {
+  fetchNodeIntrospectionData: PropTypes.func.isRequired,
   nodes: ImmutablePropTypes.map.isRequired,
   nodesInProgress: ImmutablePropTypes.set.isRequired
 };
