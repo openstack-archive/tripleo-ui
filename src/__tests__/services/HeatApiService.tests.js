fada9c5d1aff687044b72c5b84da6af324ebd8f4
Jiri Tomasek <jtomasek@redhat.com>
2017-07-21 15:22:36 +0200

    HeatApiService error handling
    * updates HeatApiService and actions that use it, to support new error
      handling
    
    Change-Id: I62a8de027aa0f395ffdcdb56687ca980d851a00b

--- src/__tests__/services/HeatApiService.tests.js
+++ src/__tests__/services/HeatApiService.tests.js
@@ -22,16 +22,18 @@ import HeatApiService from '../../js/services/HeatApiService';
 describe('HeatApiService', () => {
   describe('.getStacks() (success)', () => {
     const apiResponse = {
-      stacks: [
-        { stack_name: 'overcloud', stack_status: 'CREATE_COMPLETE' },
-        { stack_name: 'anothercloud', stack_status: 'CREATE_FAILED' }
-      ]
+      data: {
+        stacks: [
+          { stack_name: 'overcloud', stack_status: 'CREATE_COMPLETE' },
+          { stack_name: 'anothercloud', stack_status: 'CREATE_FAILED' }
+        ]
+      }
     };
     let result;
 
     beforeEach(done => {
       spyOn(utils, 'getServiceUrl').and.returnValue('example.com');
-      spyOn(HeatApiService, 'request').and.returnValue(
+      spyOn(HeatApiService, 'defaultRequest').and.returnValue(
         when.resolve(apiResponse)
       );
       HeatApiService.getStacks().then(res => {
@@ -41,7 +43,12 @@ describe('HeatApiService', () => {
     });
 
     it('returns a stack object based on <planName>', () => {
-      expect(result).toEqual(apiResponse);
+      expect(result).toEqual({
+        stacks: [
+          { stack_name: 'overcloud', stack_status: 'CREATE_COMPLETE' },
+          { stack_name: 'anothercloud', stack_status: 'CREATE_FAILED' }
+        ]
+      });
     });
   });
 });
