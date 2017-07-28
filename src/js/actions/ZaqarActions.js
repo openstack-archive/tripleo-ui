6e07e9de2df23d0ecccc9398fdace746f4c5868c
Honza Pokorny <honza@redhat.com>
2017-07-28 18:38:07 +0200

    Add 'discover and enroll nodes' workflow
    Change-Id: I334a262a36e996eb2f713c612d8018b1b78a7780
    Implements: blueprint node-discovery-by-range

--- src/js/actions/ZaqarActions.js
+++ src/js/actions/ZaqarActions.js
@@ -50,6 +50,10 @@ export default {
           );
           break;
 
+        case MistralConstants.BAREMETAL_DISCOVER_AND_ENROLL:
+          dispatch(NodesActions.discoverAndEnrollNodesFinished(payload));
+          break;
+
         case MistralConstants.BAREMETAL_INTROSPECT:
           dispatch(NodesActions.nodesIntrospectionFinished(payload));
           break;
