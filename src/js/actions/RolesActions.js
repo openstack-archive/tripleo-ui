6e2b4fa2efbaa826cb2bc3c19b699562f5ed5917
Jiri Tomasek <jtomasek@redhat.com>
2017-10-06 16:42:24 +0200

    Select Roles feature
    * Available Roles actions, selectors and reducer
    * SelectRolesDialog, Form and AvailableRoleInput
    * NodesAssignment reorganization and update
    * FloatingToolbar component
    
    Implements: blueprint tripleo-ui-select-roles
    Depends-On: I23087bd38c3730c9f24e03e4d7c54688ae912b08
    Change-Id: I1f30c46fa78b05a4dd06d657596327e680add608

--- src/js/actions/RolesActions.js
+++ src/js/actions/RolesActions.js
@@ -62,5 +62,56 @@ export default {
     return {
       type: RolesConstants.FETCH_ROLES_FAILED
     };
+  },
+
+  fetchAvailableRoles(planName) {
+    return (dispatch, getState) => {
+      dispatch(this.fetchAvailableRolesPending());
+      dispatch(
+        MistralApiService.runWorkflow(MistralConstants.LIST_AVAILABLE_ROLES, {
+          container: planName
+        })
+      ).catch(error => {
+        dispatch(handleErrors(error, 'Available Roles could not be loaded'));
+        dispatch(this.fetchAvailableRolesFailed());
+      });
+    };
+  },
+
+  fetchAvailableRolesFinished(payload, history) {
+    return (dispatch, getState, { getIntl }) => {
+      const { formatMessage } = getIntl(getState());
+      debugger;
+      if (payload.status === 'SUCCESS') {
+        const roles = normalize(response, arrayOf(roleSchema)).entities
+          .roles || {};
+        dispatch(this.fetchAvailableRolesSuccess(roles));
+      } else {
+        dispatch(
+          handleErrors(payload.message, 'Available Roles could not be loaded')
+        );
+        history.push('/plans');
+        dispatch(this.fetchAvailableRolesFailed());
+      }
+    };
+  },
+
+  fetchAvailableRolesPending() {
+    return {
+      type: RolesConstants.FETCH_AVAILABLE_ROLES_PENDING
+    };
+  },
+
+  fetchAvailableRolesSuccess(roles) {
+    return {
+      type: RolesConstants.FETCH_AVAILABLE_ROLES_SUCCESS,
+      payload: roles
+    };
+  },
+
+  fetchAvailableRolesFailed() {
+    return {
+      type: RolesConstants.FETCH_AVAILABLE_ROLES_FAILED
+    };
   }
 };
