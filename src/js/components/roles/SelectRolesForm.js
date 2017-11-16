f77392dc147223c6aef4a974fad2ea58087a0b5d
Jiri Tomasek <jtomasek@redhat.com>
2017-11-16 16:19:39 +0100

    Submit select roles
    * Change FormErrorList to render standard alert
    * Add SelectRolesForm validation
    * Add actions for selecting roles and handling zaqar message
    
    Implements: blueprint tripleo-ui-select-roles
    Depends-On: I42d66da37dcb07c1be112623d89769377bb23284
    Change-Id: I1305b5cb1522ca18f03c4c5113b822a86aff5d97

--- src/js/components/roles/SelectRolesForm.js
+++ src/js/components/roles/SelectRolesForm.js
@@ -16,37 +16,61 @@
 
 import { Button } from 'react-bootstrap';
 import { defineMessages, FormattedMessage, injectIntl } from 'react-intl';
-import { ModalBody, ModalFooter } from 'react-bootstrap';
+import { ModalFooter } from 'react-bootstrap';
+import { pickBy } from 'lodash';
+import { OverlayLoader } from '../ui/Loader';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { reduxForm } from 'redux-form';
 
 import { CloseModalButton } from '../ui/Modals';
-import FormErrorList from '../ui/forms/FormErrorList';
+import ModalFormErrorList from '../ui/forms/ModalFormErrorList';
 
 const messages = defineMessages({
   saveChanges: {
-    id: 'SelectRolesDialog.saveChanges',
+    id: 'SelectRolesForm.saveChanges',
     defaultMessage: 'Save Changes'
   },
   cancel: {
-    id: 'SelectRolesDialog.cancel',
+    id: 'SelectRolesForm.cancel',
     defaultMessage: 'Cancel'
+  },
+  updatingRoles: {
+    id: 'SelectRolesForm.updatingRoles',
+    defaultMessage: 'Updating Roles...'
+  },
+  primaryRoleValidationError: {
+    id: 'SelectRolesForm.primaryRoleValidationError',
+    defaultMessage:
+      'Please select one role tagged as "primary" and "controller"'
   }
 });
 
 class SelectRolesForm extends React.Component {
   render() {
-    const { children, error, handleSubmit, invalid, pristine } = this.props;
+    const {
+      children,
+      error,
+      handleSubmit,
+      invalid,
+      intl: { formatMessage },
+      pristine,
+      submitting
+    } = this.props;
     return (
       <form onSubmit={handleSubmit}>
-        <ModalBody>
-          <FormErrorList errors={error ? [error] : []} />
-          {children}
-        </ModalBody>
+        <OverlayLoader
+          loaded={!submitting}
+          content={formatMessage(messages.updatingRoles)}
+        >
+          <ModalFormErrorList errors={error ? [error] : []} />
+          <div className="cards-pf">
+            <div className="row row-cards-pf">{children}</div>
+          </div>
+        </OverlayLoader>
         <ModalFooter>
           <Button
-            disabled={invalid || pristine}
+            disabled={invalid || pristine || submitting}
             bsStyle="primary"
             type="submit"
           >
@@ -65,14 +89,29 @@ SelectRolesForm.propTypes = {
   currentPlanName: PropTypes.string.isRequired,
   error: PropTypes.object,
   handleSubmit: PropTypes.func.isRequired,
+  intl: PropTypes.object.isRequired,
   invalid: PropTypes.bool.isRequired,
-  pristine: PropTypes.bool.isRequired
+  pristine: PropTypes.bool.isRequired,
+  submitting: PropTypes.bool.isRequired
+};
+
+const validateForm = (values, { availableRoles }) => {
+  const errors = {};
+  const selectedRoleNames = Object.keys(pickBy(values));
+  const selectedRoles = availableRoles.filter((r, k) =>
+    selectedRoleNames.includes(k)
+  );
+  if (!selectedRoles.some(r => r.tags.includes('primary'))) {
+    errors._error = {
+      message: <FormattedMessage {...messages.primaryRoleValidationError} />
+    };
+  }
+  return errors;
 };
 
 const form = reduxForm({
-  enableReinitialize: true,
   form: 'selectRoles',
-  keepDirtyOnReinitialize: true
+  validate: validateForm
 });
 
 export default injectIntl(form(SelectRolesForm));
