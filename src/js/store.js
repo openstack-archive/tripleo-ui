5fbcccaeabe80387648df9e96130b41cc3127f53
Julie Pichon <jpichon@redhat.com>
2018-01-18 10:51:55 +0000

    (Do not merge) Testing undercloud job
    Change-Id: Ia89f5741b3852a7a17e33fa5347242708bf2246c

--- src/js/store.js
+++ src/js/store.js
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-
+// test
 import { applyMiddleware, createStore } from 'redux';
 import cookie from 'react-cookie';
 import { fromJS } from 'immutable';
