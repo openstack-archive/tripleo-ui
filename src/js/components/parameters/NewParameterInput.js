0ddb9a69d88821db8146a9e0cf39c12c9d21a011
Jiri Tomasek <jtomasek@redhat.com>
2018-01-19 11:46:25 +0100

    Convert Parameters form to use redux-form
    Using Formsy in large forms causes performance problems
    
    * Introduce ParametersForm and ParametersSidebar
    * Add NewParameterInputList and NewParameterInput as the old ones are
      still used in role forms. After those are converted too, we'll rename
      these components
    * Validate json parameter inputs
    * Handle submitting of ParametersForm
    * Separate updateParameters action to 3 actions: updateParameters,
      updateNodesAssignment, updateRoleParameters as those 3 dialogs all
      update parameters using the same mistral action but they handle it
      differently
    
    Implements: blueprint ui-redux-form-migration
    Closes-Bug: 1745408
    Change-Id: I527f3587f8adce89be649c487d9a0df5ce4939d5

---
+++ src/js/components/parameters/NewParameterInput.js
@@ -0,0 +1,121 @@
+/**
+ * Copyright 2018 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { Field } from 'redux-form';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { isObjectLike } from 'lodash';
+
+import HorizontalInput from '../ui/reduxForm/HorizontalInput';
+import HorizontalTextarea from '../ui/reduxForm/HorizontalTextarea';
+import HorizontalCheckBox from '../ui/reduxForm/HorizontalCheckBox';
+import HorizontalStaticText from '../ui/forms/HorizontalStaticText';
+
+// TODO(jtomasek): rename this when original Formsy based ParameterInputList and
+// ParameterInput are not used
+class NewParameterInput extends React.Component {
+  render() {
+    const { name, label, description, defaultValue, value, type } = this.props;
+    const commonProps = {
+      component: HorizontalInput,
+      name,
+      id: name,
+      label,
+      description,
+      labelColumns: 4,
+      inputColumns: 8
+    };
+    if (value) {
+      return (
+        <HorizontalStaticText
+          text={isObjectLike(value) ? JSON.stringify(value) : value}
+          title={label}
+          labelColumnClasses="col-sm-4"
+          inputColumnClasses="col-sm-8"
+        />
+      );
+    } else if (type.toLowerCase() === 'commadelimitedlist') {
+      return (
+        <Field
+          {...commonProps}
+          parse={value => value.split(',')}
+          component={HorizontalTextarea}
+        />
+      );
+    } else if (type.toLowerCase() === 'json' || isObjectLike(defaultValue)) {
+      return <Field {...commonProps} component={HorizontalTextarea} />;
+    } else if (
+      type.toLowerCase() === 'string' &&
+      /^.*(Key|Cert|Certificate)$/.test(name)
+    ) {
+      return <Field {...commonProps} component={HorizontalTextarea} />;
+    } else if (type.toLowerCase() === 'number') {
+      return (
+        <Field
+          {...commonProps}
+          type="number"
+          parse={value =>
+            isNaN(parseInt(value)) ? undefined : parseInt(value)
+          }
+        />
+      );
+    } else if (type.toLowerCase() === 'boolean') {
+      return (
+        <Field
+          {...commonProps}
+          type="checkbox"
+          component={HorizontalCheckBox}
+        />
+      );
+    } else {
+      return (
+        <Field
+          {...commonProps}
+          component={HorizontalInput}
+          description={description}
+          labelColumns={4}
+          inputColumns={8}
+        />
+      );
+    }
+  }
+}
+NewParameterInput.propTypes = {
+  defaultValue: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.array,
+    PropTypes.bool,
+    PropTypes.number,
+    PropTypes.string
+  ]),
+  description: PropTypes.string.isRequired,
+  intl: PropTypes.object,
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.array,
+    PropTypes.bool,
+    PropTypes.number,
+    PropTypes.string
+  ])
+};
+NewParameterInput.defaultProps = {
+  defaultValue: ''
+};
+
+export default NewParameterInput;
---
+++ src/js/components/parameters/ParametersForm.js
@@ -0,0 +1,166 @@
+/**
+ * Copyright 2018 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { Button, Form, ModalFooter } from 'react-bootstrap';
+import { defineMessages, FormattedMessage, injectIntl } from 'react-intl';
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import { isEqual, pickBy } from 'lodash';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { reduxForm } from 'redux-form';
+
+import { CloseModalButton } from '../ui/Modals';
+import ModalFormErrorList from '../ui/forms/ModalFormErrorList';
+import { OverlayLoader } from '../ui/Loader';
+
+const messages = defineMessages({
+  cancel: {
+    id: 'ParametersForm.cancel',
+    defaultMessage: 'Cancel'
+  },
+  saveChanges: {
+    id: 'ParametersForm.saveChanges',
+    defaultMessage: 'Save Changes'
+  },
+  saveAndClose: {
+    id: 'ParametersForm.saveAndClose',
+    defaultMessage: 'Save And Close'
+  },
+  updatingParameters: {
+    id: 'ParametersForm.updatingParameters',
+    defaultMessage: 'Updating parameters'
+  },
+  enterValidJson: {
+    id: 'ParametersForm.enterValidJson',
+    defaultMessage: 'Please enter a valid JSON.'
+  },
+  invalidParameters: {
+    id: 'ParametersForm.invalidParameters',
+    defaultMessage: 'Some parameter values are invalid:',
+    description: 'Form error notification title'
+  },
+  invalidParametersList: {
+    id: 'ParametersForm.invalidParametersList',
+    defaultMessage: '{parameters}',
+    description: 'A list of invalid parameters in form error message'
+  }
+});
+
+const ParametersForm = ({
+  dispatch,
+  error,
+  children,
+  onSubmit,
+  handleSubmit,
+  intl: { formatMessage },
+  invalid,
+  pristine,
+  submitting,
+  initialValues
+}) => (
+  <Form onSubmit={handleSubmit} horizontal>
+    <OverlayLoader
+      loaded={!submitting}
+      content={formatMessage(messages.updatingParameters)}
+    >
+      <ModalFormErrorList errors={error ? [error] : []} />
+      {children}
+    </OverlayLoader>
+    <ModalFooter>
+      <Button
+        type="submit"
+        disabled={invalid || pristine || submitting}
+        bsStyle="primary"
+      >
+        <FormattedMessage {...messages.saveChanges} />
+      </Button>
+      <Button
+        disabled={invalid || pristine || submitting}
+        onClick={handleSubmit(values =>
+          onSubmit({ ...values, saveAndClose: true }, dispatch, {
+            initialValues
+          })
+        )}
+      >
+        <FormattedMessage {...messages.saveAndClose} />
+      </Button>
+      <CloseModalButton>
+        <FormattedMessage {...messages.cancel} />
+      </CloseModalButton>
+    </ModalFooter>
+  </Form>
+);
+ParametersForm.propTypes = {
+  children: PropTypes.node,
+  dispatch: PropTypes.func.isRequired,
+  error: PropTypes.object,
+  handleSubmit: PropTypes.func.isRequired,
+  initialValues: PropTypes.object.isRequired,
+  intl: PropTypes.object.isRequired,
+  invalid: PropTypes.bool.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  parameters: ImmutablePropTypes.map.isRequired,
+  pristine: PropTypes.bool.isRequired,
+  submitting: PropTypes.bool.isRequired
+};
+
+const isJSON = value => {
+  try {
+    // empty string is considered valid JSON type parameter value
+    return value === '' ? true : !!JSON.parse(value);
+  } catch (e) {
+    return false;
+  }
+};
+
+const validate = (
+  values,
+  { initialValues, parameters, intl: { formatMessage } }
+) => {
+  const errors = {};
+
+  // Validate only parameters which were updated by User
+  const updatedValues = pickBy(
+    values,
+    (value, key) => !isEqual(value, initialValues[key])
+  );
+
+  Object.keys(updatedValues).map(key => {
+    if (parameters.getIn([key, 'type']).toLowerCase() === 'json') {
+      if (!isJSON(values[key])) {
+        errors[key] = formatMessage(messages.enterValidJson);
+      }
+    }
+  });
+
+  // Add global error message
+  if (Object.keys(errors).length > 0) {
+    errors['_error'] = {
+      title: formatMessage(messages.invalidParameters),
+      message: formatMessage(messages.invalidParametersList, {
+        parameters: Object.keys(errors).join(', ')
+      })
+    };
+  }
+  return errors;
+};
+
+const form = reduxForm({
+  form: 'parametersForm',
+  validate
+});
+
+export default injectIntl(form(ParametersForm));
