be2664740a2c19968214f8e2270208bb922b1b17
Honza Pokorny <honza@redhat.com>
2018-02-09 12:05:05 -0400

    Add flavor fetching via Nova API
    This patch support for fetching Nova flavors via its REST API.  We're
    including all of the necessary pieces including reducer, selector, etc.
    
    Change-Id: I9535fa8254039ef564d62b653934267b2380ce7c
    Depends-On: Ic6df58947bb2cb1e183b5c88ed8d287191e5ee07

---
+++ src/js/services/NovaApiService.js
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2018 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+import axios from 'axios';
+import when from 'when';
+
+import { AuthenticationError, NovaApiError, ConnectionError } from './errors';
+import { getAuthTokenId, getServiceUrl } from '../selectors/auth';
+
+class NovaApiService {
+  defaultRequest(path, additionalAttributes) {
+    return (dispatch, getState) =>
+      axios(
+        Object.assign(
+          {
+            baseURL: getServiceUrl(getState(), 'nova'),
+            url: path,
+            method: 'GET',
+            headers: {
+              'X-Auth-Token': getAuthTokenId(getState())
+            }
+          },
+          additionalAttributes
+        )
+      );
+  }
+
+  getFlavors() {
+    return dispatch =>
+      dispatch(this.defaultRequest('/flavors'))
+        .then(response => response.data)
+        .catch(handleErrors);
+  }
+}
+
+const handleErrors = e => {
+  if (e.response && e.response.status === 401) {
+    return when.reject(new AuthenticationError(e));
+  } else if (e.response) {
+    return when.reject(new NovaApiError(e));
+  } else if (e.request) {
+    return when.reject(
+      new ConnectionError('Connection to Nova API could not be established', e)
+    );
+  } else {
+    return when.reject(e);
+  }
+};
+
+export default new NovaApiService();
