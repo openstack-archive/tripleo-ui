29fefe11e0d537e2d6bcd80ffaa31f99faf60316
Jiri Tomasek <jtomasek@redhat.com>
2018-02-23 15:10:23 +0100

    [WIP] Add Network Topology diagram
    Add components to construct network topology diagram
    
    Implements: blueprint tripleo-ui-configure-networks
    Co-Authored-By: Honza Pokorny <honza@redhat.com>
    Change-Id: I16f48e7d52d8c96c1bcbb7a0d28553500334c10d

---
+++ src/__tests__/utils/immutable.tests.js
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2018 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { List } from 'immutable';
+import { splitListIntoChunks } from '../../js/utils/immutablejs';
+
+describe('splitListIntoChunks function', () => {
+  it('works on empty lists', () => {
+    const list = List();
+    expect(splitListIntoChunks(list, 3)).toEqual(List());
+  });
+
+  it('works when list length and chunk length is the same', () => {
+    const list = List([1, 2, 3]);
+    expect(splitListIntoChunks(list, 3)).toEqual(
+      List([List([1]), List([2]), List([3])])
+    );
+  });
+
+  it('works when list length is a multiple of chunk size ', () => {
+    const list = List([1, 2, 3, 4]);
+    expect(splitListIntoChunks(list, 2)).toEqual(
+      List([List([1, 2]), List([3, 4])])
+    );
+  });
+
+  it('works when the result are uneven chunks', () => {
+    const list = List([1, 2, 3, 4]);
+    expect(splitListIntoChunks(list, 3)).toEqual(
+      List([List([1, 2]), List([3, 4])])
+    );
+    expect(splitListIntoChunks(List([1, 2, 3, 4, 5, 6, 7]), 3)).toEqual(
+      List([List([1, 2, 3]), List([4, 5, 6]), List([7])])
+    );
+  });
+});
---
+++ src/js/components/networkConfiguration/RoleCard.js
@@ -0,0 +1,75 @@
+/**
+ * Copyright 2018 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+import RoleNetworkLine from './RoleNetworkLine';
+import { getNetworkColorStyle } from './utils';
+
+export class RoleCard extends Component {
+  state = { roleBottomY: 0 };
+  componentDidMount() {
+    this.setState({ roleBottomY: this.getRoleBottomY(this.element) });
+  }
+
+  getRoleBottomY(roleCardElement) {
+    const rect = roleCardElement.getBoundingClientRect();
+    return rect.y + rect.height;
+  }
+
+  render() {
+    const { name, networks, networkLinePositions } = this.props;
+    return (
+      <div
+        className="card-pf card-pf-view card-pf-view-select card-pf-accented"
+        ref={el => (this.element = el)}
+      >
+        <div className="card-pf-body">
+          <h2 className="card-pf-title">{name}</h2>
+          <ul className="role-networks-list list-unstyled">
+            {networks.map(network => (
+              <RoleNetworkLine
+                key={network}
+                style={{
+                  height: networkLinePositions[network]
+                    ? networkLinePositions[network] - this.state.roleBottomY
+                    : 0,
+                  bottom: networkLinePositions[network]
+                    ? -networkLinePositions[network] +
+                      this.state.roleBottomY -
+                      2
+                    : 0,
+                  backgroundColor: getNetworkColorStyle(network)
+                    .backgroundColor,
+                  borderColor: getNetworkColorStyle(network).borderColor
+                }}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+RoleCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  networkLinePositions: PropTypes.object,
+  networks: ImmutablePropTypes.list.isRequired
+};
+
+export default RoleCard;
