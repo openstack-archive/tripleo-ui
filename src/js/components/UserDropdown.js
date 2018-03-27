1374cb9dbe56e287f5a87de471d55f7380883970
Liz Blanchard <lblanchard@redhat.com>
2018-03-27 13:38:33 -0400

    Move Logout under User Name Dropdown
    This patch cleans up the navigation a bit and tucks the logout
    action under a new dropdown that shows the username. This will
    help reduce any errant logouts and follow the PatternFly logout
    pattern. This change fixes one bug and partially fixes another.
    
    Change-Id: I525af00f4c121fcae7f186d1a4d61ade8d8b33f6
    Closes-Bug: #1727501
    Partial-Bug: #1691113

---
+++ src/js/components/UserDropdown.js
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2017 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+import React from 'react';
+import { defineMessages, FormattedMessage, injectIntl } from 'react-intl';
+
+import Dropdown from '../components/ui/dropdown/Dropdown';
+import DropdownToggle from '../components/ui/dropdown/DropdownToggle';
+import DropdownItem from '../components/ui/dropdown/DropdownItem';
+
+import PropTypes from 'prop-types';
+import ImmutablePropTypes from 'react-immutable-proptypes';
+
+const messages = defineMessages({
+  logout: {
+    id: 'UserDropdown.logout',
+    defaultMessage: 'Logout'
+  }
+});
+
+class UserDropdown extends React.Component {
+  render() {
+    return (
+      <Dropdown>
+        <DropdownToggle>
+          <span className="pficon pficon-user" /> {this.props.name}
+          <b className="caret" />
+        </DropdownToggle>
+        <DropdownItem onClick={this.props.logout}>
+          <FormattedMessage {...messages.logout} />
+        </DropdownItem>
+      </Dropdown>
+    );
+  }
+}
+
+UserDropdown.propTypes = {
+  logout: PropTypes.func.isRequired,
+  name: ImmutablePropTypes.map
+};
+
+export default injectIntl(UserDropdown);
