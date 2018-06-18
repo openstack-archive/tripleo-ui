f5fdb030567ea0733f17d3e0374491870b0c4376
Jiri Tomasek <jtomasek@redhat.com>
2018-06-18 13:31:25 +0200

    Warn when some network resources are not available
    In case when network-isolation is not enabled, network topology diagram
    shows networks-roles assigment as usual but hightlights networks which
    don't have corresponding Heat resource defined. Notification about network
    isolation is desplayed to the user.
    
    Implements: blueprint networks-crud-ui
    Change-Id: Id4b8e3e1c4b8a7de9e463230d822bbbb0afb321c

--- src/js/components/networkConfiguration/NetworkTopology.js
+++ src/js/components/networkConfiguration/NetworkTopology.js
@@ -14,14 +14,30 @@
  * under the License.
  */
 
+import { connect } from 'react-redux';
+import { defineMessages, FormattedMessage } from 'react-intl';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import React, { Component } from 'react';
 import { debounce } from 'lodash';
+import { Alert } from 'patternfly-react';
 
+import {
+  getNetworks,
+  getNetworkResourceExistsByNetwork
+} from '../../selectors/networks';
+import { getParameters } from '../../selectors/parameters';
+import { getRoles } from '../../selectors/roles';
 import RolesList from './RolesList';
 import NetworksList from './NetworksList';
 
-export default class NetworkTopology extends Component {
+const messages = defineMessages({
+  networkIsolationWarning: {
+    id: 'NetworkConfiguration.networkIsolationWarning',
+    defaultMessage:
+      'Resource definition for some networks is missing. Please enable Network Isolation.'
+  }
+});
+class NetworkTopology extends Component {
   constructor() {
     super();
     this.networkLineElements = {};
@@ -44,24 +60,47 @@ export default class NetworkTopology extends Component {
     });
 
   render() {
-    const { roles, networks, parameters } = this.props;
+    const {
+      roles,
+      networkResourceExistsByNetwork,
+      networks,
+      parameters
+    } = this.props;
     return (
-      <div className="network-topology">
-        <RolesList
-          roles={roles}
-          networkLinePositions={this.state.networkLinePositions}
-        />
-        <NetworksList
-          networks={networks}
-          parameters={parameters}
-          networkLineElements={this.networkLineElements}
-        />
+      <div>
+        {networkResourceExistsByNetwork.includes(false) && (
+          <Alert type="info">
+            <FormattedMessage {...messages.networkIsolationWarning} />
+          </Alert>
+        )}
+        <div className="network-topology">
+          <RolesList
+            roles={roles}
+            networkLinePositions={this.state.networkLinePositions}
+          />
+          <NetworksList
+            networks={networks}
+            networkResourceExistsByNetwork={networkResourceExistsByNetwork}
+            parameters={parameters}
+            networkLineElements={this.networkLineElements}
+          />
+        </div>
       </div>
     );
   }
 }
 NetworkTopology.propTypes = {
+  networkResourceExistsByNetwork: ImmutablePropTypes.map.isRequired,
   networks: ImmutablePropTypes.map.isRequired,
   parameters: ImmutablePropTypes.map.isRequired,
   roles: ImmutablePropTypes.map.isRequired
 };
+
+const mapStateToProps = state => ({
+  networkResourceExistsByNetwork: getNetworkResourceExistsByNetwork(state),
+  networks: getNetworks(state),
+  parameters: getParameters(state),
+  roles: getRoles(state)
+});
+
+export default connect(mapStateToProps)(NetworkTopology);
