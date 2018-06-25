0dcce58279efe26bca00e7ec708b21c24ef5775d
Jiri Tomasek <jtomasek@redhat.com>
2018-06-25 11:36:29 +0200

    List deployment failures on deployment failure
    On deployment failure, a general deployment failure message is displayed
    along with either a list of stack resources or deployment failures based
    on the deployment phase the deployment failed at
    
    Implements: blueprint config-download-ui
    Change-Id: Ide45310ebbaf428b960c7db467777e75ff755529

--- src/js/components/deployment/DeploymentFailure.js
+++ src/js/components/deployment/DeploymentFailure.js
@@ -15,24 +15,35 @@
  */
 
 import { connect } from 'react-redux';
-import { injectIntl } from 'react-intl';
+import { FormattedMessage, injectIntl, defineMessages } from 'react-intl';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { ModalBody } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 import DeleteStackButton from '../deployment_plan/DeleteStackButton';
-import RecoverDeploymentStatusButton from '../deployment/RecoverDeploymentStatusButton';
+import RecoverDeploymentStatusButton from './RecoverDeploymentStatusButton';
+import DeploymentFailures from './DeploymentFailures';
 import { deploymentStatusMessages } from '../../constants/DeploymentConstants';
+import { stackStates } from '../../constants/StacksConstants';
 import { getCurrentStack } from '../../selectors/stacks';
 import {
   getCurrentPlanDeploymentStatus,
   getCurrentPlanDeploymentStatusUI
 } from '../../selectors/deployment';
 import InlineNotification from '../ui/InlineNotification';
+import { Loader } from '../ui/Loader';
 import { sanitizeMessage } from '../../utils';
 import StacksActions from '../../actions/StacksActions';
 import { startUndeploy } from '../../actions/DeploymentActions';
+import StackResources from './StackResources';
+
+const messages = defineMessages({
+  fetchingStack: {
+    id: 'DeploymentFailure.fetchingStack',
+    defaultMessage: 'Loading deployment information...'
+  }
+});
 
 class DeploymentFailure extends React.Component {
   componentDidMount() {
@@ -45,6 +56,7 @@ class DeploymentFailure extends React.Component {
       deploymentStatus: { status, message },
       undeployPlan,
       intl: { formatMessage },
+      isFetchingStacks,
       isPendingRequest,
       planName,
       stack
@@ -52,20 +64,42 @@ class DeploymentFailure extends React.Component {
 
     return (
       <ModalBody className="flex-container">
-        <InlineNotification
-          type="error"
-          title={formatMessage(deploymentStatusMessages[status], { planName })}
-        >
+        <div className="page-header">
+          <div className="pull-right">
+            {isFetchingStacks ||
+              (stack && (
+                <DeleteStackButton
+                  deleteStack={() => undeployPlan()}
+                  disabled={isPendingRequest}
+                />
+              ))}
+            {isFetchingStacks || (!stack && <RecoverDeploymentStatusButton />)}
+          </div>
+          <h2>
+            <FormattedMessage
+              {...deploymentStatusMessages[status]}
+              values={{ planName }}
+            />
+          </h2>
+        </div>
+        <InlineNotification type="error">
           <p>{sanitizeMessage(message)}</p>
         </InlineNotification>
-        <div>
-          {stack && (
-            <DeleteStackButton
-              deleteStack={undeployPlan.bind(this, planName)}
-              disabled={isPendingRequest}
-            />
-          )}
-          {!stack && <RecoverDeploymentStatusButton />}
+        <div className="flex-column">
+          <Loader
+            loaded={!isFetchingStacks}
+            content={formatMessage(messages.fetchingStack)}
+          >
+            {stack &&
+              ([
+                stackStates.CREATE_COMPLETE,
+                stackStates.UPDATE_COMPLETE
+              ].includes(stack.stack_status) ? (
+                <DeploymentFailures />
+              ) : (
+                <StackResources stack={stack} />
+              ))}
+          </Loader>
         </div>
       </ModalBody>
     );
@@ -90,8 +124,8 @@ const mapStateToProps = (state, props) => ({
   stack: getCurrentStack(state)
 });
 
-const mapDispatchToProps = dispatch => ({
-  undeployPlan: planName => dispatch(startUndeploy(planName)),
+const mapDispatchToProps = (dispatch, { planName }) => ({
+  undeployPlan: () => dispatch(startUndeploy(planName)),
   fetchStacks: () => dispatch(StacksActions.fetchStacks())
 });
 
